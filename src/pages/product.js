095bfb5aa96cb5eaf26c24ce0b0a7ed9f27abd5a
Guilherme Ortiz <guiortizsantos12@gmail.com>
2020-02-29 17:10:16 -0300

    Curso da Rocketseat de React-Native concluido

---
+++ src/pages/product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {WebView} from 'react-native';
+
+const Product = ({navigation}) => (
+    <WebView source={{uri:navigation.state.params.product.url}}/>
+);
+
+Product.navigationOptions = ({navigation}) => ({
+    title: navigation.state.params.product.title
+});  
+export default Product;
